77bb9e4a882ca6cceaebc82713beb1211f69bc37
TAwosanya <babatunde.awosanya@yahoo.com>
2014-05-07 18:15:57 +0100

    Files for Submission

---
+++ plot1.R
@@ -0,0 +1,16 @@
+timeStamp <- seq.POSIXt(strptime("16/12/2006 17:24:00", format="%d/%m/%Y %H:%M:%S"), strptime("26/11/2010 21:02:00", format="%d/%m/%Y %H:%M:%S"), "min")
+startDateTimeIndex <- which(timeStamp==strptime("01/02/2007", format="%d/%m/%Y"))
+endDateTimeIndex <- which(timeStamp==strptime("02/02/2007 23:59:00", format="%d/%m/%Y %H:%M:%S"))
+skip <- startDateTimeIndex
+nrows <- endDateTimeIndex - startDateTimeIndex
+tempData <- read.csv2("./exdata-data-household_power_consumption/household_power_consumption.txt", header=TRUE, nrows=1)
+myData <- read.csv2("./exdata-data-household_power_consumption/household_power_consumption.txt", header=FALSE, nrows=nrows, skip=skip)
+names(myData) <- names(tempData)
+
+myData$TimeStamp <- strptime(paste(myData[,1],myData[,2]), format="%d/%m/%Y %H:%M:%S")
+myData[,3:9] <- apply(myData[,3:9],2, as.numeric)
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+hist(myData$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.off()
+
---
+++ plot4.R
@@ -0,0 +1,23 @@
+timeStamp <- seq.POSIXt(strptime("16/12/2006 17:24:00", format="%d/%m/%Y %H:%M:%S"), strptime("26/11/2010 21:02:00", format="%d/%m/%Y %H:%M:%S"), "min")
+startDateTimeIndex <- which(timeStamp==strptime("01/02/2007", format="%d/%m/%Y"))
+endDateTimeIndex <- which(timeStamp==strptime("02/02/2007 23:59:00", format="%d/%m/%Y %H:%M:%S"))
+skip <- startDateTimeIndex
+nrows <- endDateTimeIndex - startDateTimeIndex
+tempData <- read.csv2("./exdata-data-household_power_consumption/household_power_consumption.txt", header=TRUE, nrows=1)
+myData <- read.csv2("./exdata-data-household_power_consumption/household_power_consumption.txt", header=FALSE, nrows=nrows, skip=skip)
+names(myData) <- names(tempData)
+
+myData$TimeStamp <- strptime(paste(myData[,1],myData[,2]), format="%d/%m/%Y %H:%M:%S")
+myData[,3:9] <- apply(myData[,3:9],2, as.numeric)
+
+png(filename = "plot4.png", width = 480, height = 480, units = "px")
+op <- par(mfrow=c(2,2))
+with(myData, plot(TimeStamp, Global_active_power, type="l", ylab="Global Active Power", xlab=""))
+with(myData, plot(TimeStamp, Voltage, type="l", ylab="Voltage", xlab="datetime"))
+with(myData, plot(TimeStamp, Sub_metering_1, type="l", ylab="Energy sub metering", xlab=""))
+with(myData, lines(TimeStamp, Sub_metering_2, col="red"))
+with(myData, lines(TimeStamp, Sub_metering_3, col="blue"))
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","red","blue"), lty=c(1,1,1))
+with(myData, plot(TimeStamp, Global_reactive_power, type="l", ylab="Global_reactive_power", xlab="datetime"))
+op <- par(op)
+dev.off()
